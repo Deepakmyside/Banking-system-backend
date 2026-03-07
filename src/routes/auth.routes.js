73d71065fb889ed681641d7d142c682f3d849415
Deepak Sharma <deepakmysidegmail.com>
2026-03-07 10:57:37 +0530

    Initial backend setup of our banking system backend project

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const authController = require("../controllers/auth.controller")
+
+const router = express.Router()
+console.log("Auth routes loaded")
+/*Post /api/auth/register */
+router.post("/register", authController.userRegisterController)
+    
+/*Post /api/auth/login*/
+router.post("/login", authController.userLoginController )
+console.log("login route hit");
+module.exports = router 
