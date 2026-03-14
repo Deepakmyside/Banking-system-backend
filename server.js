3f82f40a19bf1c6a93011f098c18b166dcda83b0
Deepak Sharma <deepakmyside@gmail.com>
2026-03-15 00:10:21 +0530

    Dynamic port code added

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ require("dotenv").config()
 const app = require("./src/app")
 const connectToDB = require("./src/config/db")
 
-
+const PORT = process.env.PORT || 3000
 connectToDB()
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is running on port is 3000 ")
 })
