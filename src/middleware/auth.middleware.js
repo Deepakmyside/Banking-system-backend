865a782dc79d87956440bf8c65e168c6f318b5f7
Deepak Sharma <deepakmyside@gmail.com>
2026-03-14 15:41:25 +0530

    system user Middleware added for validating systemUser

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -5,7 +5,8 @@ const jwt = require("jsonwebtoken")
 async function authMiddleware(req, res, next) {
     const token = req.cookies.token || req.headers.authorization?.split(" ") [ 1 ]
 
-   if(!token) res.status(402).json({
+   if(!token) 
+    return res.status(402).json({
     message: "Unauthorization access, token is missing"
    })
 
@@ -23,6 +24,38 @@ try {
     })
 }
 }
+
+async function authSystemUserMiddleware (req, res, next ) {
+    const token = req.cookies.token || req.headers.authorization?.split (" ") [ 1 ]
+
+    if(!token)
+        return  res.status(401).json({
+    message: "Unauthorized access, token is missing"
+        })
+    }
+    
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        const user = await userModel.findById(decoded.userId).select("+systemUser")
+        if(!user.systemUser) {
+            return res.status(403).json({
+                message: "Forbidden access, not a system user"
+            })
+        }
+        req.user = user
+        return next()
+
+    }
+    catch(error){
+      return res.status(401).json({
+        message: "Unauthorized access,token is invalid"
+      })
+    }
+
+
+
 module.exports = {
-    authMiddleware
+    authMiddleware,
+    authSystemUserMiddleware
 }
